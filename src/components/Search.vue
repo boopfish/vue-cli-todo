82fc572f468c41bc39ebbc38609c2b1947f17039
yanan <yanan@deloitte.com.cn>
2022-02-21 11:14:25 +0800

    axios demo  实现发送请求渲染数据

---
+++ src/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="out-container">
+    <input type="text" ref="inputText">
+    <button class="btn btn-primary" @click="search">search</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Search",
+  methods: {
+    search() {
+      axios.get(`https://api.github.com/search/users?q=${this.$refs.inputText.value}`)
+          .then(response => {
+                console.log(response);
+                this.$bus.$emit("getUserList", response.data);
+              },
+              error => {
+                console.log(error.message);
+              }
+          );
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/UserList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <li  v-for="item in items" :key="item.id">
+      <span>{{item.login}}</span>
+      <img :src="item.avatar_url" :alt="item.html_url">
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserList",
+  data() {
+    return {
+      items: []
+    }
+  },
+  mounted() {
+    this.$bus.$on("getUserList", (data) => {
+      //console.log("UserList组件接收到数据", data)
+      this.items = data.items;
+    })
+  }
+}
+</script>
+
+<style scoped>
+ img {
+   width: 50px;
+   height: 50px;
+ }
+
+</style>
