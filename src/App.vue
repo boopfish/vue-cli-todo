9ad89604aa4bf713cd4dc2b0582f6426b0633b49
yanan <yanan@deloitte.com.cn>
2022-02-21 12:35:03 +0800

    默认插槽

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <template>
   <div id="app">
-    <search/>
-    <UserList/>
+    <Category :listData="foods" title="美食">
+      <img src="logo.png" alt="">
+    </Category>
+    <Category :listData="films" title="电影">
+      <li v-for="data in films" :key="data">{{ data }}</li>
+    </Category>
+    <Category :listData="books" title="图书">
+      <li v-for="data in books" :key="data">{{ data }}</li>
+    </Category>
   </div>
 </template>
 
 <script>
-import Search from "./components/Search"
-import UserList from "./components/UserList"
+import Category from "./components/Category"
 
 export default {
   name: 'App',
-  components: {Search, UserList},
+  components: {Category},
   data() {
-    return {}
+    return {
+      foods: ['红烧牛肉', '蒜泥白肉', '糖醋里脊', '小炒肉'],
+      films: ['奇迹', '长津湖', '这个杀手不太冷静', '忠犬八公'],
+      books: ['Java从入门到入坟', 'JavaScript从入门到入坟', 'Vue从入门到入坟']
+    }
   }
 }
 </script>
 
 <style>
+img {
+  width: 100px;
+  height: 100px;
+}
 </style>
---
+++ src/components/Category.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h3>{{ title }}</h3>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props: [ 'listData','title']
+}
+</script>
+
+<style scoped>
+.container {
+  background-color: pink;
+  height: 300px;
+  width: 200px;
+  float: left;
+  margin-left: 50px;
+  text-align: center;
+}
+li{
+  text-align: left;
+}
+
+</style>
