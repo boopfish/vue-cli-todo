b5eb186b7b0fe015c0beea6efcb6c423ba203b69
yanan <yanan@deloitte.com.cn>
2022-02-22 17:48:21 +0800

    initialize branch vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <template>
   <div id="app">
-    <Counter/>
-    <hr/>
-    <Person/>
   </div>
 </template>
 
 <script>
-import Counter from "./components/Counter";
-import Person from "./components/Person";
 
 export default {
   name: 'App',
   components: {
-    Counter, Person
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
 
 Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
-  store,
   beforeCreate() {
     Vue.prototype.$bus = this; //配置全局事件总线 $bus就是当前应用的vm
   }
